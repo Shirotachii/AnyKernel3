2249066b80d97241313d163550dacbfacf760902
Shiro <222724628+Shirotachii@users.noreply.github.com>
2025-09-09 15:57:25 +0700

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,42 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Kurap1ka Kernel For Redmi Note 9 by @vq6oon
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=maguro
+device.name2=toro
+device.name3=toroplus
+device.name4=tuna
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+  set_perm_recursive 0 0 755 644 $ramdisk/*;
+  set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+## boot shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot;
+
+write_boot;
+## end boot install
